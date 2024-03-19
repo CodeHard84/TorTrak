22ffd1918dccb22a796bac5f4149c7640319f8b1
Jace <sockgnome@att.net>
2024-03-19 09:21:24 -0500

    Added base data for #8. Source NOAA.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
 // TorTrak Source
 // Filename: app.js
+
+'use strict';
+
+function AppSate() {
+  this.storms = [];
+}
+
+AppSate.prototype.loadStorms = function () {
+  // Load all storms from storms.json if
+  // allStorms key does not exist in storage.
+};
